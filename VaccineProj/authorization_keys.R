2dbe99ec25d764d7464561a760d426b258942288
lakshnorris <140061597+lakshnorris@users.noreply.github.com>
2023-07-31 11:32:04 +0530

    removed auth_data

--- VaccineProj/authorization_keys.R
+++ VaccineProj/authorization_keys.R
@@ -2,5 +2,5 @@
 get_auth_data <- function(auth_data) {
   # ... (fictional safety data analysis, e.g., calculating adverse event rates)
 
-  return("This is the flag: CAREFULLWITHCOMMITS")
+  return(auth_data.key)
 }
