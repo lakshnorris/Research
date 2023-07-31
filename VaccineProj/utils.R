5c36ad5d88ba4a336797d66e9946cbd7c90120d9
lakshnorris <140061597+lakshnorris@users.noreply.github.com>
2023-07-31 11:41:47 +0530

    updated data format for reporting

--- VaccineProj/utils.R
+++ VaccineProj/utils.R
@@ -7,6 +7,6 @@
 # Sample fictional utility function
 format_output <- function(data) {
   # ... (fictional formatting of output)
-
+  formatted_output = data_preprocessing.formatting(data.model_result)
   return(formatted_output)
 }
