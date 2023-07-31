97f9f8e192743d7952958632d1c5d153a13fc255
lakshnorris <140061597+lakshnorris@users.noreply.github.com>
2023-07-31 11:34:36 +0530

    updated geographic analysis

--- VaccineProj/geographic_analysis.R
+++ VaccineProj/geographic_analysis.R
@@ -2,6 +2,7 @@
 
 # Function to analyze vaccine efficacy across different geographic locations
 analyze_geographic_data <- function(location_data, efficacy_data) {
+  geographic_results = location_data.vaccine + efficacy_data.vaccine
   # ... (fictional geographic analysis, e.g., regional efficacy rates)
 
   return(geographic_results)
