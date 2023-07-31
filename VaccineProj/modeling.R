c4dbd75319af4192de0f87aa24010b0151260970
lakshnorris <140061597+lakshnorris@users.noreply.github.com>
2023-07-31 11:39:30 +0530

    upated predictive model

--- VaccineProj/modeling.R
+++ VaccineProj/modeling.R
@@ -3,6 +3,7 @@
 # Function to build predictive models
 build_predictive_model <- function(data) {
   # ... (fictional model building)
-
+  inter_model_result = build_predictive_model(data.Visualization)
+  model_result =  inter_model_result.Modeling(Visualization)
   return(model_result)
 }
